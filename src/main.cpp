7c0f629150eaa9efe6055ea6efc077a7e2d2da50
Ethan Lu <elu@nevada.unr.edu>
2019-09-10 14:23:02 -0400

    add optimo optimization library

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,18 @@
 #include "args/args.hxx"
 #include "imgui.h"
 
+#include "optimo/solvers/sparse_primal_dual_qp.h"
+
 using namespace geometrycentral;
 using namespace geometrycentral::surface;
 using std::cout;
 using std::endl;
 using std::unique_ptr;
+
+using optimo::solvers::SparsePrimalDualQP;
+
+using Eigen::Matrix;
+using Eigen::Dynamic;
 
 // == Geometry-central data
 unique_ptr<HalfedgeMesh> mesh;
@@ -22,9 +29,12 @@ unique_ptr<VertexPositionGeometry> geometry;
 // Mesh data
 size_t nVertices;
 size_t nEdges;
+size_t nCorners;
 EdgeData<size_t> eInd;
 VertexData<size_t> vInd;
+CornerData<size_t> cInd;
 CornerData<double> cornerAngles;
+VertexData<double> angleDefects; 
 
 // Optimization Parameters
 double t = 2.;
@@ -33,8 +43,10 @@ double epsilon = 0.001;
 
 // Optimization Stuff
 SparseMatrix<double> constraints;
+Vector<double> x_init;
 Vector<double> x;
 Vector<double> rhs;
+Vector<double> ineqRHS;
 
 // Polyscope visualization handle, to quickly add data to the surface
 polyscope::SurfaceMesh *psMesh;
@@ -44,12 +56,14 @@ void generateConstraints()
     // Initialization
     nVertices = mesh->nVertices();
     nEdges = mesh->nEdges();
+    nCorners = mesh->nCorners();
     eInd = mesh->getEdgeIndices();
     vInd = mesh->getVertexIndices();
+    cInd = mesh->getCornerIndices();
     geometry->requireVertexGaussianCurvatures();
-    VertexData<double> angleDefects = geometry->vertexGaussianCurvatures;
+    angleDefects = geometry->vertexGaussianCurvatures;
 
-    // Matrix initialization
+    // Equality constraint initialization
     constraints = SparseMatrix<double>(nVertices, nEdges);
     std::vector<Eigen::Triplet<double>> tripletList;
     rhs = Vector<double>(nVertices);
@@ -79,8 +93,8 @@ bool checkInequalityConstraints()
     for (Corner c : mesh->corners())
     {
         Halfedge h = c.halfedge();
-        netAngles[c] += x[eInd[h.edge()]];
-        netAngles[h.next().corner()] += x[eInd[h.edge()]];
+        netAngles[c] += x_init[eInd[h.edge()]];
+        netAngles[h.next().corner()] += x_init[eInd[h.edge()]];
     }
     for (Corner c : mesh->corners())
     {
@@ -92,84 +106,127 @@ bool checkInequalityConstraints()
     return true;
 }
 
-void barrierOpt()
-{
-}
-
-inline double logBarrier(const double xi, const double upperBound)
-{
-    return -(1 / t) * log(-(upperBound - xi));
-}
-
-inline void vecProj(SparseMatrix<double> A, Vector<double> xi, Vector<double> b)
-{
-}
-double objFunction()
-{
-    return 0.;
-}
-
-int main(int argc, char **argv)
+void generateVisualization()
 {
 
-    // Configure the argument parser
-    args::ArgumentParser parser("geometry-central & Polyscope example project");
-    args::Positional<std::string> inputFilename(parser, "mesh", "A mesh file.");
-
-    // Parse args
-    try
-    {
-        parser.ParseCLI(argc, argv);
-    }
-    catch (args::Help)
-    {
-        std::cout << parser;
-        return 0;
-    }
-    catch (args::ParseError e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-    // Make sure a mesh name was given
-    if (!inputFilename)
-    {
-        std::cerr << "Please specify a mesh file as argument" << std::endl;
-        return EXIT_FAILURE;
-    }
-    // Initialize polyscope
-    polyscope::init();
-
-    // Load mesh
-    std::tie(mesh, geometry) = loadMesh(args::get(inputFilename));
-    generateConstraints();
-    // Initialize with simple linear solve
-    // This will screw up if SuiteSparse doesn't exist
-    x = solve(constraints, rhs);
-    if (checkInequalityConstraints())
-    {
-        cout << "No log-barrier required!" << endl;
-    }
-    else
-    {
-        cout << "Doing log-barrier now..." << endl;
-        barrierOpt();
-    }
-
-    // Register the mesh with polyscope
-    psMesh = polyscope::registerSurfaceMesh(
-        polyscope::guessNiceNameFromPath(args::get(inputFilename)),
-        geometry->inputVertexPositions, mesh->getFaceVertexList(),
-        polyscopePermutations(*mesh));
-
-    // Visualization
-    psMesh->addEdgeScalarQuantity("Alphas", x);
-    psMesh->addVertexScalarQuantity("curvature",
-                                  geometry->vertexGaussianCurvatures);
-
-    // Give control to the polyscope gui
-    polyscope::show();
-
-    return EXIT_SUCCESS;
 }
+
+void optimizationStep()
+{ 
+    // Initialization
+    // parameters are number of unknowns, number of inequality contraints, number of eq constraints
+    const uint n = nEdges;
+    const uint m = 2*nCorners;
+    const uint p = nVertices;
+    const uint l = n + m + p;
+    SparsePrimalDualQP<double>::Params params(n, m, p);
+    double min_value;
+    params.reserve();
+    params.d = Vector<double>::Zero(nEdges,1);
+    // Equality constraints
+    params.beq = rhs;
+    for (Edge e : mesh->edges())
+    {
+        params.setAeqElement(vInd[e.halfedge().vertex()], eInd[e], 1);
+        params.setAeqElement(vInd[e.halfedge().twin().vertex()], eInd[e], 1);
+    }
+
+    // Inequality constraints 
+    Matrix<double, Dynamic, 1>& bin = params.bin;   
+    for (Corner c : mesh->corners())
+    {
+        bin[cInd[c]] = 2*PI - geometry->cornerAngle(c);
+        bin[cInd[c] + nCorners] = -geometry->cornerAngle(c);
+        Halfedge h = c.halfedge();
+        params.setAinElement(cInd[c], eInd[h.edge()] , 1);
+        params.setAinElement(cInd[c] + nCorners, eInd[h.edge()] , -1);
+        params.setAinElement(cInd[h.next().corner()], eInd[h.edge()] , 1);
+        params.setAinElement(cInd[h.next().corner()] + nCorners, eInd[h.edge()] , -1);
+
+    }
+
+    // Optimization variable lol
+    for (uint i = 0; i < n; i++) 
+    {     
+        params.setQElement(i, i, 1);
+    }
+
+    // Plug in initial guess, I think?
+    Matrix<double, Dynamic, 1> y(l);
+    for (uint i = 0; i < n; i++) y(0, i) = x_init[i];
+    SparsePrimalDualQP<double> qp_solver;   
+    auto res = qp_solver(&params, &y, &min_value);
+    x = y.block(0, 0, n, 1).transpose();
+    cout << "RESULT: " << res << endl;
+    cout << "VALUE: " << min_value << endl;
+    cout << "Optimization Done" << endl;
+}
+
+
+
+
+    int main(int argc, char **argv)
+    {
+
+        // Configure the argument parser
+        args::ArgumentParser parser("geometry-central & Polyscope example project");
+        args::Positional<std::string> inputFilename(parser, "mesh", "A mesh file.");
+
+        // Parse args
+        try
+        {
+            parser.ParseCLI(argc, argv);
+        }
+        catch (args::Help)
+        {
+            std::cout << parser;
+            return 0;
+        }
+        catch (args::ParseError e)
+        {
+            std::cerr << e.what() << std::endl;
+            std::cerr << parser;
+            return 1;
+        }
+        // Make sure a mesh name was given
+        if (!inputFilename)
+        {
+            std::cerr << "Please specify a mesh file as argument" << std::endl;
+            return EXIT_FAILURE;
+        }
+        // Initialize polyscope
+        polyscope::init();
+
+        // Load mesh
+        std::tie(mesh, geometry) = loadMesh(args::get(inputFilename));
+        generateConstraints();
+        // Initialize with simple linear solve
+        // This will screw up if SuiteSparse doesn't exist
+        x_init = solve(constraints, rhs);
+        if (checkInequalityConstraints())
+        {
+            cout << "No optimization required!" << endl;
+            x = x_init;
+        }
+        else
+        {
+            cout << "Doing optimization now..." << endl;
+            optimizationStep();
+        }
+
+        // Register the mesh with polyscope
+        psMesh = polyscope::registerSurfaceMesh(
+                polyscope::guessNiceNameFromPath(args::get(inputFilename)),
+                geometry->inputVertexPositions, mesh->getFaceVertexList(),
+                polyscopePermutations(*mesh));
+
+        // Visualization
+        psMesh->addEdgeScalarQuantity("Alphas", x);
+        psMesh->addVertexScalarQuantity("curvature",
+                geometry->vertexGaussianCurvatures);
+
+        // Give control to the polyscope gui
+        polyscope::show();
+
+        return EXIT_SUCCESS;
+    }
