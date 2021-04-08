3ede7019c3e7c514422a7879260b9800fd2804e9
Ethan Lu <elu@nevada.unr.edu>
2021-04-07 21:27:26 -0400

    flattening optimization

--- src/main.cpp
+++ src/main.cpp
@@ -128,18 +128,19 @@ void generateConstraints() {
     rhs = vector<double>(nVertices);
     for (size_t i = 0; i < nVertices; i++) {
         if (mesh-> vertex(i).isBoundary()) {
-            rhs[i] = PI - geometry->vertexAngleSums[mesh->vertex(i)]/2;
+            rhs[i] = PI - geometry->vertexAngleSums[mesh->vertex(i)];
         } else {
-            rhs[i] =  angleDefects[mesh->vertex(i)] / 2.;                
+            rhs[i] =  angleDefects[mesh->vertex(i)];                
         }
     }
     for (Edge e : mesh->edges()) {
+        double weight = e.isBoundary() ? 1. : 2. ;
         rows.emplace_back(vInd[e.halfedge().vertex()]);
         cols.emplace_back(eInd[e]);
-        values.emplace_back(1.);
+        values.emplace_back(weight);
         rows.emplace_back(vInd[e.halfedge().twin().vertex()]);
         cols.emplace_back(eInd[e]);
-        values.emplace_back(1.);
+        values.emplace_back(weight);
     }
     auto r = new_array_ptr<int>(rows);
     auto c = new_array_ptr<int>(cols);
@@ -572,9 +573,84 @@ void dbgOutput(string filename) {
         s << vInd[e.halfedge().vertex()] << " " << vInd[e.halfedge().twin().vertex()] << "\n";
     }
 }
+inline double shift(double c) {
+    return (c + 2.) * 500;
+}
+void dbgSVG(string filename) {
+    std::ofstream ss(filename, std::ofstream::out);
+    ss << "<?xml version=\"1.0\" standalone=\"no\" ?>" << endl
+       << "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" << endl
+       << "<svg width=\"2000\" height=\"2000\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >" << endl;
+    for (Vertex v : mesh->vertices()) {
+        auto thing = flattened[vInd[v]];
+        ss << "<circle cx=\"" << shift(thing.x) << "\" cy=\"" << shift(thing.y) << "\" r=\"1\"/>" << endl;
+    }
+    for (Edge e : mesh->edges()) {
+        Vector2 i = flattened[vInd[e.halfedge().vertex()]];
+        Vector2 j = flattened[vInd[e.halfedge().twin().vertex()]];
+        double angle = alphas[eInd[e]];
+        // FROM NORMALIZATION
+        double radius = 500 * norm(i - j) / abs(2 * sin(angle));
+        if (abs(angle) > 1e-7) {
+            string largeArcFlag = std::abs(2 * angle) <= 3.14159265358979323846264 ? "0" : "1";
+            // sweep flag is 1 if going outward, 0 if going inward
+            string sweepFlag = angle < 0 ? "0" : "1";
+            ss << "<path d=\"M" << shift(i.x) << "," << shift(i.y) << " A" << radius << ","
+            << radius << " 0 " << largeArcFlag << " " << sweepFlag << " "
+            << shift(j.x) << "," << shift(j.y) << "\" fill=\"none\" stroke=\"green\" stroke-width=\"2\" />" << endl;
+        } else {
+            ss << "<line x1=\"" << shift(i.x) << "\" x2=\"" << shift(j.x) 
+            << "\" y1=\"" << shift(i.y) <<"\" y2=\"" << shift(j.y) << "\" stroke=\"blue\" stroke-width=\"2\"/>" << endl;
+        }
+        
+    }
+    // footer
+    ss << "</svg>";
+}
 
 double confObjective(vector<Vector2> &flattened, vector<double>& alphas) {
     double residual = 0.;
+    bool first = true;
+    for (Vertex v: mesh->vertices()) {
+        double accum = 0.;
+        double targetAccum = 0;
+        //double vAngle = geometry->vertexAngleSums[v];
+        for (Corner C : v.adjacentCorners()) {
+            targetAccum += targetAngles[C];
+            Halfedge h = C.halfedge();
+            Halfedge ab = h;
+            Halfedge bc = h.next();
+            Halfedge ca = h.next().next();
+            if (h.isInterior()) {
+                const Vector2 &a = flattened[vInd[ab.vertex()]];
+                const Vector2 &b = flattened[vInd[bc.vertex()]];
+                const Vector2 &c = flattened[vInd[ca.vertex()]];
+                auto u = b - a;
+                auto v = c - a;
+                double angle = orientedAngle(u,v);
+                // alphas are offsets from canonical halfedge orientation
+                if (h.edge().halfedge() == h) {
+                    angle += alphas[eInd[h.edge()]];
+                } else {
+                    angle -= alphas[eInd[h.edge()]];
+                }
+                if (ca.edge().halfedge() == ca) {
+                    angle += alphas[eInd[ca.edge()]];
+                } else {
+                    angle -= alphas[eInd[ca.edge()]];
+                }
+
+                accum += angle;
+            }
+            
+        }
+        if (first) {
+            if (v.isBoundary()) {
+                //cout << "Actual boundary sum: " << accum << "target: " << targetAccum << endl;
+            } 
+            first = false;
+        }
+    }
     //for (Corner C: mesh->corners()) cout << targetAngles[C] << endl;
     double result = 0.;
     for (Corner C: mesh->corners()) {
@@ -602,6 +678,9 @@ double confObjective(vector<Vector2> &flattened, vector<double>& alphas) {
             }
             //cout << "target:" << targetAngles[C] << "   " << "actual:" << angle << endl;
             // check: sign issue?
+            if (ab.vertex().isBoundary()) {
+                //cout << "BOUNDARY " << angle << " target " << targetAngles[C] << endl;
+            }
             result += sqr(angle - targetAngles[C]);
         }
     }
@@ -673,8 +752,12 @@ void confStep(int n) {
     //while (sqrt(grad_size) > ep) {
         double result = confObjective(flattened, alphas);
         confGradient(flattened, alphas, flattened_grad, alphas_grad);
-        grad_size = 1.0; // change this
-        double t = .1;
+        grad_size = 0; // change this
+        for (Vector2 a: flattened_grad) grad_size += a.norm2();
+        for (double a: alphas_grad) grad_size += a*a;
+        
+        //double t = .00001;
+        double t = 0.01;
         int steps = 0;
         while (confObjective(flattened_new, alphas_new) > result - alpha * t * grad_size) {
             steps++;
@@ -687,16 +770,26 @@ void confStep(int n) {
                 alphas_new[i] = alphas[i] - t * alphas_grad[i];
                 //cout << "a" << alphas_grad[i] << endl;
             }
+            /*
+            int i = 6;
+            flattened_new[i] = flattened[i] + Vector2{t,0};
+            //alphas_new[i] = alphas[i] + t;
+            double new_result = confObjective(flattened_new, alphas_new);
+            cout << "Objective change:" << new_result - result << endl;
+            cout <<  "Estimated change:" << t *(flattened_grad[i].x) << endl;
+            */
         }
-        cout << "t: " << t << endl;
-        cout << "steps: " << steps << endl;
+        
         flattened = flattened_new;
         alphas = alphas_new;
-        if (true) {
+        if (iter % 1000 == 0) {
             cout << "Starting iteration " << iter << endl;
+            cout << "t: " << t << endl;
+            cout << "grad" << grad_size << endl;
+            cout << "steps: " << steps << endl;
             //cout << "grad size squared:" << grad_size << endl;
             cout << "objective:" << result << endl;
-            dbgOutput("chug" + std::to_string(iter + 1));
+            dbgSVG("step" + std::to_string(iter + 1) + ".svg");
             //std::ofstream s ("chug" + std::to_string(iter), std::ofstream::out);
         }
         /*
@@ -705,7 +798,6 @@ void confStep(int n) {
             dbgOutput("final");
         }
         */
-        cout << "objective:" << result << endl;
         iter++;
     }
     //cout << "iteration count: " << iter << endl;
@@ -816,22 +908,40 @@ void conformalFlatten() {
             angle += sol[eInd[h.edge()]];
             angle += sol[eInd[h.next().next().edge()]];
             targetAngles[C] = angle;
+            //cout << "angle is" << angle;
         }
     }
+    /*
     // DEBUG
     for (Vertex v: mesh->vertices()) {
         double accum = geometry->vertexAngleSums[v];
         for (Edge e: v.adjacentEdges()) {
-            accum += 2*sol[eInd[e]];
+            accum += (e.isBoundary() ? 1 : 2)*sol[eInd[e]];
+        }
+        if (v.isBoundary()) {
+            cout << "boundary from solve" << accum << endl;
+        } else {
+            cout << "not boundary from solve" << accum << endl;
+        }
+    }
+    
+    // DEBUG
+    for (Vertex v: mesh->vertices()) {
+        double accum = 0;
+        //double vAngle = geometry->vertexAngleSums[v];
+        for (Corner C : v.adjacentCorners()) {
+            accum += targetAngles[C];
         }
         if (v.isBoundary()) {
             cout << "boundary" << accum << endl;
         } else {
-            cout << "not boundary" << accum << endl;
+           cout << "not boundary" << accum << endl;
         }
     }
-    dbgOutput("chug0");
-    confStep(5000);
+    */
+    dbgSVG("step0.svg");
+    //dbgOutput("chug0");
+    confStep(50000);
     /*
     for (bff::WedgeCIter w: model[0].cutBoundary()) {
             w->uv;
@@ -839,7 +949,42 @@ void conformalFlatten() {
     */        
 
 }
+// just for validating the SVG formula I'm using
+void testSVG() {
+    std::ofstream ss("test.svg", std::ofstream::out);
+    ss << "<?xml version=\"1.0\" standalone=\"no\" ?>" << endl
+       << "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" << endl
+       << "<svg width=\"2000\" height=\"2000\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >" << endl;
+    ss << "<circle cx=\"" << 500 << "\" cy=\"" << (500) << "\" r=\"1\"/>" << endl;
+    ss << "<circle cx=\"" << 500 << "\" cy=\"" << (1000) << "\" r=\"1\"/>" << endl;
 
+    Vector2 i = {500, 500};
+    Vector2 j = {1000, 500};
+    double angle = 3*PI/4;
+    double radius = norm(i - j) / abs(2 * sin(angle));
+    string largeArcFlag = std::abs(2 * angle) <= 3.14159265358979323846264 ? "0" : "1";
+    // sweep flag is 1 if going outward, 0 if going inward
+    string sweepFlag = angle < 0 ? "0" : "1";
+    ss << "<path d=\"M" << (i.x) << "," << (i.y) << " A" << radius << ","
+       << radius << " 0 " << largeArcFlag << " " << sweepFlag << " "
+       << (j.x) << "," << (j.y) << "\" fill=\"none\" stroke=\"green\" stroke-width=\"2\" />" << endl;
+    ss << "<line x1=\"" << (i.x) << "\" x2=\"" << (j.x)
+       << "\" y1=\"" << (i.y) << "\" y2=\"" << (j.y) << "\" stroke=\"blue\" stroke-width=\"2\"/>" << endl;
+    
+    double wog = 1.3;
+    j = {500 + 700*cos(wog), 500 + 800*sin(wog)};
+    angle = -(PI - wog - angle);
+    radius = norm(i - j) / abs(2 * sin(angle));
+    largeArcFlag = std::abs(2 * angle) <= 3.14159265358979323846264 ? "0" : "1";
+    // sweep flag is 1 if going outward, 0 if going inward
+    sweepFlag = angle < 0 ? "0" : "1";
+    ss << "<path d=\"M" << (i.x) << "," << (i.y) << " A" << radius << ","
+       << radius << " 0 " << largeArcFlag << " " << sweepFlag << " "
+       << (j.x) << "," << (j.y) << "\" fill=\"none\" stroke=\"green\" stroke-width=\"2\" />" << endl;
+    ss << "<line x1=\"" << (i.x) << "\" x2=\"" << (j.x)
+       << "\" y1=\"" << (i.y) << "\" y2=\"" << (j.y) << "\" stroke=\"blue\" stroke-width=\"2\"/>" << endl;
+    ss << "</svg>";
+}
 int main(int argc, char **argv) {
 
     // Configure the argument parser
@@ -863,7 +1008,7 @@ int main(int argc, char **argv) {
     if (!inputFilename) {
         //inputMeshPath = "/home/elu/repos/catopt/meshes/spotwithhole.obj";
         //inputMeshPath = "/home/elu/repos/catopt/meshes/cube.obj";
-        inputMeshPath = "/home/elu/repos/catopt/meshes/patch.obj";
+        inputMeshPath = "/home/elu/repos/catopt/meshes/nonconvex2.obj";
         //std::cerr << "Please specify a mesh file as argument" << std::endl;
         //return EXIT_FAILURE;
     } else {
@@ -871,8 +1016,8 @@ int main(int argc, char **argv) {
     }
     // Initialize polyscope
     cout << "Initialized" << endl;
-    polyscope::init("openGL_mock");
-    //polyscope::init();
+    //polyscope::init("openGL_mock");
+    polyscope::init();
 
     polyscope::state::userCallback = myCallback;
     // Load mesh
@@ -895,6 +1040,7 @@ int main(int argc, char **argv) {
     //////////////////////////////////////////
     // BFF stuff
     conformalFlatten();
+    //testSVG();
 
     //generateVisualization();
     // Give control to the polyscope gui
